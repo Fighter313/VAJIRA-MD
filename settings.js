ce29d38cf1583a18143ab4aae1760504c4e0e840
Fighter313 <164225642+Fighter313@users.noreply.github.com>
2024-07-22 22:40:32 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> U M A R I -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '923308363433' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=KFF10CpT#UgyEC7cSCfOZj2hZXFqixz2c2GaaKM3jt9L2YSnOIlE' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=6FF1URDa#eMVv8MCcuK6E94euwRekQLpKohMVMwA6-1Tc5za73z0' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "923308363433,923308363433" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
